4919e73d68ea70b0a319227179199ef284fdc550
unknown <matthew.chandler@TST-MATTC.red-gate.com>
2016-01-06 16:21:29 +0000

    Adding another row of static data

--- AdventureWorksSSDT/AdventureWorksSSDT/dbo/Tables/t.refdata.sql
+++ AdventureWorksSSDT/AdventureWorksSSDT/dbo/Tables/t.refdata.sql
@@ -3,6 +3,7 @@
 insert into t (id, dita) values (1, 'hello!')
 insert into t (id, dita, furtherData) values (10, 'hello mum', null)
 insert into t (id, dita, furtherData) values (11, 'wahey', 'hurray')
+insert into t (id, dita, furtherData) values (12, 'wahey', 'hurray')
 
 --insert into MattTable (id, data, furtherData) values (1, 2, 'three')
 
